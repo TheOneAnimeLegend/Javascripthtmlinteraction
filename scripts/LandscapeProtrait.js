c1bdc20f52a46cd8e234b68d9e02109846130aad
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-10-18 13:08:49 -0600

    Update

--- scripts/LandscapeProtrait.js
+++ scripts/LandscapeProtrait.js
@@ -1,1 +1,44 @@
-console.log("You made it to the LandscapeProtrait.js!")
+//console.log("You made it to the LandscapeProtrait.js!")
+
+let stop = false;
+
+function testNaN (number) {
+  console.log(stop);
+  if ( isNaN (number) {
+    stop = true;
+    return "Type a Real Number"
+  } else {
+    return "Input validated"
+  }
+}
+
+function geometry (width, height) {
+
+// This coding illustrated the advanced method
+function main (// get the value of the width input field
+firstNumber = document.getElementById("textfield1").value;
+
+
+//valuidate if user typed a number, odd validation by TRUE conditional
+document.getElementbyId("validiytest1").innerHTML = alert( testNaN (firstNumber);
+document.getElementbyId("validiytest1").innerHTML = testNaN (firstNumber);
+
+
+// Get the value of the Height Input Field, Id="textfield2"
+secondNumber = document.getElementById("textfield2").value;
+
+//valuidate if user typed a number, odd validation by TRUE conditional
+document.getElementbyId("validiytest2").innerHTML = alert( testNaN (secondNumber);
+document.getElementbyId("validiytest2").innerHTML = testNaN (secondNumber);
+
+if (stop == true) {
+  document.getElementById("large").innerHTML = "restart because I said so."
+}
+else {
+  console.log("what did you say."), geometry (firstNumber, secondNumber));
+  document.getElementById("large").innerHTML = "what did you say" + geometry (firstNumber, secondNumber);
+ }
+}
+) {
+
+     let (testNumber,)
--- scripts/childTemplate.js
+++ scripts/childTemplate.js
@@ -1,1 +1,31 @@
 console.log("You made it to the childtemplate.js!")
+
+function main () {=
+
+  let firstNumber, secondNumber, text;
+
+// get the value of the width input field
+firstNumber = document.getElementById("textfield").value;
+
+
+//valuidate if user typed a number, odd validation by TRUE conditional
+document.getElementbyId("validiytest").innerHTML = alert( testNaN (firstNumber);
+document.getElementbyId("validiytest").innerHTML = testNaN (firstNumber);
+
+
+// Get the value of the Height Input Field, Id="textfield2"
+secondNumber = document.getElementById("textfield2").value;
+
+//valuidate if user typed a number, odd validation by TRUE conditional
+document.getElementbyId("validiytest").innerHTML = alert( testNaN (secondNumber);
+document.getElementbyId("validiytest").innerHTML = testNaN (secondNumber);
+
+if (stop == true) {
+  document.getElementById("large").innerHTML = "restart because I said so."
+}
+else {
+  console.log("what did you say."), geometry (firstNumber, secondNumber));
+  document.getElementById("large").innerHTML = "what did you say" + geometry (firstNumber, secondNumber);
+
+ }
+}
